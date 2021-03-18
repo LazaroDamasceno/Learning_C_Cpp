fe959e828d51275edc10e85adb705f3e0bcbad01
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2021-03-18 00:31:52 -0300

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int factorial(int num)
+{
+    if(num<=1)
+    {
+        return 1;
+    }
+    return num*factorial(num-1);
+}
+
+int main()
+{
+    int num;
+    cin >> num;
+    cout << factorial(num) << endl;
+    return 0;
+}
