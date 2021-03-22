dee5095439c5e78901dea37aa74e1efbd36dbf0a
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2021-03-22 16:52:28 -0300

    Add files via upload

---
+++ to_string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+    int sumone = 11;
+    int sumtwo = 12;
+    cout << to_string(sumone) + to_string(sumtwo) << "\n";
+}
+
