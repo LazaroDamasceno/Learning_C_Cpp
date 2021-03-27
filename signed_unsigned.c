62d6aba0f47db1a4f7529117aa162b54d116642d
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2021-03-27 14:24:31 -0300

    Add files via upload

---
+++ signed_unsigned.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned int a = 12;
+    signed int b = -12;
+    unsigned int c = -12;
+    signed int d = 12;
+    printf("%d\n%d\n%d\n%d\n", a, b, c, d);
+}
