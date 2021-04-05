61c6125eddb9419a184d179fc1281dee7de6371e
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2021-04-05 09:36:42 -0300

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -8,7 +8,7 @@ int main(void)
 {
     vector<int>v{12,4,7,3,2,11,7,9,1,0,10};
     sort(v.begin(), v.end());
-    for(int i=0; i<v.size(); ++i)
-        cout << v[i] << " ";
+    for(int x : v)
+        cout << x << " ";
     cout << endl;
 }
