9e148539fa0d90f7586845fc443916263a51a2f9
Lázaro Lawrence Pereira Damasceno <62726118+LazaroDamasceno@users.noreply.github.com>
2021-04-05 10:09:39 -0300

    Update string_char_foreach_loop.cpp

--- string_char_foreach_loop.cpp
+++ string_char_foreach_loop.cpp
@@ -15,3 +15,10 @@ int main(void)
     for(char x : y)
         cout << x << " ";
     cout << endl;
+      /* Third */
+    char p[10] = {'0','1','2','3','4','5','6',
+    '7','8','9'};
+    for(char x : p)
+        cout << x << " ";
+    cout << endl;
+}
